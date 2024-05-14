1378b8e9742c742ec69bb4c1b02f3e180e967a3f
Balaramansethu <baalaaraman@gmail.com>
2024-05-15 00:03:03 +0530

    ADD. added navigation for application

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import DisplayBooksComponent from './components/DisplayBooksComponent/DisplayBooksComponent'
+import NavigationComponent from './components/NavigationComponent/NavigationComponent'
 
 const App = () => {
   return (
     <React.Fragment>
-      <DisplayBooksComponent/>
+      <NavigationComponent/>
     </React.Fragment>
   )
 }
--- src/components/NavigationComponent/NavigationComponent.jsx
+++ src/components/NavigationComponent/NavigationComponent.jsx
@@ -5,13 +5,19 @@ import BookGenreComponent from "../BookGenreComponent/BookGenreComponent";
 const NavigationComponent = () => {
   return (
     <Router>
-      <ul>
-        <Link to="/">Home</Link>
-        <Link to="/BookGenre"></Link>
-      </ul>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/BookGenre">Book genre</Link>
+          </li>
+        </ul>
+      </div>
       <Routes>
-        <Route exact path="/" element={<DisplayBooksComponent/>}/>
-        <Route exact path="/BookGenre" element={<BookGenreComponent/>}/>
+        <Route exact path="/" element={<DisplayBooksComponent />} />
+        <Route exact path="/BookGenre" element={<BookGenreComponent />} />
       </Routes>
     </Router>
   );
