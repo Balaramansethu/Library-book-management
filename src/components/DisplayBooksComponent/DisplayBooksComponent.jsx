cc29ac8c2b383126a9621a33e512452a6565dadb
Balaramansethu <baalaaraman@gmail.com>
2024-05-15 00:30:35 +0530

    ADD. added pagination for homePage

--- src/components/DisplayBooksComponent/DisplayBooksComponent.jsx
+++ src/components/DisplayBooksComponent/DisplayBooksComponent.jsx
@@ -1,20 +1,72 @@
-import React from 'react'
-import BookData from '../../data/BookData'
+import React, { useState } from "react";
+import BookData from "../../data/BookData";
 
 const DisplayBooksComponent = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const booksPerPage = 10;
+  const LastIndex = currentPage * booksPerPage;
+  const firstIndex = LastIndex - booksPerPage;
+  const books = BookData.slice(firstIndex, LastIndex);
+  const totPage = Math.ceil(BookData.length / booksPerPage);
+  const numbers = [...Array(totPage + 1).keys()].slice(1);
+
+  function previousPage(){
+    if(currentPage!== 1){
+        setCurrentPage(currentPage-1)
+    }
+  }
+
+  function changeCPage(id){
+    setCurrentPage(id)
+  }
+
+  function nextPage(){
+    if(currentPage!== totPage)
+        {
+            setCurrentPage(currentPage+1)
+        }
+  }
 
   return (
-    <div className='box-container'>
-        {BookData.map((data,index)=>(
-           <div className='card-container'>
-           <p key={index}>{data.title}</p>
-           <p key={index}>{data.author}</p>
-           <p key={index}>{data.subject}</p>
-           <p key={index}>{data.publish_date}</p>
-           </div>
-           ))}
-    </div>
-  )
-}
+    <div className="box-container">
+      {books.map((data, index) => (
+        <div className="card-container">
+          <p key={index}>Title : {data.title}</p>
+          <p key={index}>Author : {data.author}</p>
+          <p key={index}>Subject : {data.subject}</p>
+          <p key={index}>Published date : {data.publish_date}</p>
+        </div>
+      ))}
+      <footer>
+        <nav>
+          <ul>
+            <li>
+              <a href="#" onClick={previousPage}>
+                {" "}
+                Previous
+              </a>
+            </li>
+            {numbers.map((n, i) => (
+              <li
+                className={`page-item ${currentPage === n ? "active" : ""}`}
+                key={i}
+              >
+                <a href="#" className="page-item" onClick={()=>changeCPage(n)}>
+                  {n}
+                </a>
+              </li>
+            ))}
 
-export default DisplayBooksComponent
+            <li className="page-item">
+              <a href="#" className="page-link" onClick={nextPage}>
+                Next
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </footer>
+    </div>
+  );
+};
+
+export default DisplayBooksComponent;
--- src/components/PaginationComponent/PaginationComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PaginationComponent = () => {
-  return (
-    <div>PaginationComponent</div>
-  )
-}
-
-export default PaginationComponent
